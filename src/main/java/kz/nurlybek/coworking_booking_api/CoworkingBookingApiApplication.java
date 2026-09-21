3e1260a9016452cbeba25b57d43729084d4afb27
Nurlybek Nurmukhan <nnzh2001@gmail.com>
2026-09-21 14:44:33 +0500

    chore: initialize Spring Boot project

---
+++ src/main/java/kz/nurlybek/coworking_booking_api/CoworkingBookingApiApplication.java
@@ -0,0 +1,13 @@
+package kz.nurlybek.coworking_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/nurlybek/coworking_booking_api/CoworkingBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.nurlybek.coworking_booking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
